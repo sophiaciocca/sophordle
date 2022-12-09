a762fd5d1625a7233e36e61d23be5c3909e39c55
Sophia Ciocca <sophiaciocca@gmail.com>
2022-12-09 15:32:49 -0500

    adjust gameboard for past guesses + current guess to fill in squares

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -4,9 +4,22 @@ import { GUESSES_ALLOWED, WORD_LENGTH } from './App';
 function Gameboard(props) {
   console.log('IN GAMEBOARD. PROPS: ', props);
   const { currentGuess, pastGuesses } = props;
+  const remainingEmptyRows = GUESSES_ALLOWED - 1 - pastGuesses.length;
   return (
     <div className="gameboard">
-      {[...Array(GUESSES_ALLOWED)].map((arrayElem, rowIndex) => (
+      {pastGuesses.map((pastGuess, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {pastGuess.map((letter, letterIndex) => (
+            <div className="letterbox" key={letterIndex}>{letter}</div>
+          ))}
+        </div>
+      ))}
+      <div className="row">
+        {currentGuess.map((letter, letterIndex) => (
+          <div className="letterbox" key={letterIndex}>{letter}</div>
+        ))}
+      </div>
+      {[...Array(remainingEmptyRows)].map((arrayElem, rowIndex) => (
         <div className="row" key={rowIndex}>
           {[...Array(WORD_LENGTH)].map((arrayElem, boxIndex) => (
             <div className="letterbox" id={`letterbox-${rowIndex}-${boxIndex}`} key={boxIndex}></div>
