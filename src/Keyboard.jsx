e935dc7698a56862438c6235304dc9dc096f6060
Sophia Ciocca <sophiaciocca@gmail.com>
2022-12-09 12:13:44 -0500

    create basic keyboard UI w map

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -1,12 +1,25 @@
 import './Keyboard.css';
 import { GUESSES_ALLOWED, WORD_LENGTH } from './gameLogic';
 
+const letters = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+];
+
 function Keyboard() {
   return (
     <div className="keyboard">
-        KEYBOARD HERE
+      {letters.map((rowLetters, rowIndex) => (
+        <div className="keyboard-row" index={rowIndex}>
+          {rowLetters.map((letter, letterIndex) => (
+            <button className="keyboard-button" index={letterIndex}>{letter}</button>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Keyboard;
+
