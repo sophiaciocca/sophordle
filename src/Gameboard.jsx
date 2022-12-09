24a8412712dbfbda86ef60cef24b0817d6a29961
Sophia Ciocca <sophiaciocca@gmail.com>
2022-12-09 11:49:36 -0500

    delete obsolete CSS, add good CSS; have boxes now

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -4,15 +4,15 @@ import { GUESSES_ALLOWED, WORD_LENGTH } from './gameLogic';
 
 function Gameboard() {
   return (
-        <div className="gameboard">
-            {[...Array(GUESSES_ALLOWED)].map((arrayElem, rowIndex) => (
-              <div className="row" key={rowIndex}>
-                {[...Array(WORD_LENGTH)].map((arrayElem, boxIndex) => (
-                  <div className="letterbox" key={boxIndex}>letter</div>
-                ))}
-              </div>
+    <div className="gameboard">
+      {[...Array(GUESSES_ALLOWED)].map((arrayElem, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {[...Array(WORD_LENGTH)].map((arrayElem, boxIndex) => (
+            <div className="letterbox" key={boxIndex}>l</div>
           ))}
         </div>
+      ))}
+    </div>
   );
 }
 
