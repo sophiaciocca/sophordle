a1b87474449b621b551e67871ad02a8519331be7
Sophia Ciocca <sophiaciocca@gmail.com>
2022-12-13 15:50:14 -0500

    change existing test to relevant header render

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/Sophordle/i);
+  expect(headerText).toBeInTheDocument();
 });
